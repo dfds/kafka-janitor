6a21bf0a9f71e6408069ec7fbeef3a09ce288c78
Emil H. Clausen <sequoiia@hummel.yt>
2020-01-21 14:01:51 +0100

    Added Ccloud topic repository

---
+++ src/KafkaJanitor.WebApp/Infrastructure/Messaging/TopicCcloudRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KafkaJanitor.WebApp.Models;
+using Tika.Client;
+
+namespace KafkaJanitor.WebApp.Infrastructure.Messaging
+{
+    public class TopicCcloudRepository : ITopicRepository
+    {
+        private readonly ITikaClient _tikaClient;
+
+        public TopicCcloudRepository(ITikaClient tikaClient)
+        {
+            _tikaClient = tikaClient;
+        }
+        public async Task<IEnumerable<Topic>> GetAll()
+        {
+            var topics = await _tikaClient.GetTopics();
+            return topics.Select(val => new Topic(val));
+        }
+
+        public async Task Add(Topic topic)
+        {
+            await _tikaClient.CreateTopic(topic.Name, topic.Partitions.ToString());
+        }
+
+        public async Task<bool> Exists(string topicName)
+        {
+            var topics = await _tikaClient.GetTopics();
+            try
+            {
+                topics.First(to => to.Equals(topicName));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
