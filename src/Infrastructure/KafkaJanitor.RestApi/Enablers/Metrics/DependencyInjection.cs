d8f178f6d7a0ed90983eaa0673e1e026f12d9d5a
Kim Lindhard <kilin@dfds.com>
2020-02-13 05:34:15 +0100

    Move MetricHostedService from WebApp to RestApi

--- src/Infrastructure/KafkaJanitor.RestApi/Enablers/Metrics/DependencyInjection.cs
+++ src/Infrastructure/KafkaJanitor.RestApi/Enablers/Metrics/DependencyInjection.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 
-namespace KafkaJanitor.WebApp.Enablers.Metrics
+namespace KafkaJanitor.RestApi.Enablers.Metrics
 {
     public static class DependencyInjection
     {
--- src/Infrastructure/KafkaJanitor.RestApi/Enablers/Metrics/MetricHostedService.cs
+++ src/Infrastructure/KafkaJanitor.RestApi/Enablers/Metrics/MetricHostedService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Prometheus;
 
-namespace KafkaJanitor.WebApp.Enablers.Metrics
+namespace KafkaJanitor.RestApi.Enablers.Metrics
 {
     public class MetricHostedService : IHostedService
     {
