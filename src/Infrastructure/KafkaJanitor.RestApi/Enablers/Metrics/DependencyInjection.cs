ed8ed00ecbc92fa03e5de1b8949473eb6aa13c34
Kim Lindhard <42998758+kim-lindhard-dfds@users.noreply.github.com>
2020-02-27 10:14:22 +0100

    Toggle metric server (#31)

--- src/Infrastructure/KafkaJanitor.RestApi/Enablers/Metrics/DependencyInjection.cs
+++ src/Infrastructure/KafkaJanitor.RestApi/Enablers/Metrics/DependencyInjection.cs
@@ -4,8 +4,10 @@ namespace KafkaJanitor.RestApi.Enablers.Metrics
 {
     public static class DependencyInjection
     {
-        public static void AddMetrics(this IServiceCollection services)
+        public static void AddMetrics(this IServiceCollection services, bool startHostedService)
         {
+            if(startHostedService == false) {return;}
+            
             services.AddHostedService<MetricHostedService>();
         }
     }
