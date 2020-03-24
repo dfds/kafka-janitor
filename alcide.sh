d2b6f7213c5afc86653aa1022e145cb1725c89cb
Kim Lindhard <42998758+kim-lindhard-dfds@users.noreply.github.com>
2020-03-24 14:56:34 +0100

    Alcide poc (#48)
    * Initial Alcide pipeline attempt
    
    * set executable permission on alcide.sh
    
    * Specify K8s manifests for alcide script
    
    * Added debugging info to alcide.sh
    
    Added debugging info that shows the OS and platform discovered to troubleshoot curl errors
    
    * Removing os and platform debugging info
    
    Previous attempt was missing * in OS type. It was added together with debug info and seems to have fixed the issue
    
    Co-authored-by: Emil H. Clausen <sequoiia@hummel.yt>
    Co-authored-by: runeabrahamsson <5913288+runeabrahamsson@users.noreply.github.com>

---
+++ alcide.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+case "$OSTYPE" in
+  darwin*)  platform=darwin ;; 
+  linux*)   platform=linux ;;
+  #msys*)    platform=WINDOWS ;;
+  *)        (exit 1) ;;
+esac
+
+manifest=$1 # First parameter is kubernetes manifest to scan
+strict=0
+
+if [ ! -z "$2" ]
+then
+    strict=$2
+fi
+
+RC=1
+
+curl -o kube-advisor https://alcide.blob.core.windows.net/generic/DCV-2.1/$platform/advisor
+chmod +x ./kube-advisor
+
+RC=$(./kube-advisor validate file $manifest --eula-sign --outfile output.html | awk '/Critical/{print $4}')
+
+if [ $strict -gt 0 ]
+then
+RC2=$(./kube-advisor validate file $manifest --eula-sign --outfile output.html | awk '/High/{print $4}')
+RC=$(($RC + $RC2))
+fi
+
+./kube-advisor validate file $manifest --eula-sign --output yaml
+
+if [ $RC -gt 0 ]
+then
+    >&2 echo "Scan failed see output"
+    (exit 1)
+fi
